9c31af2a00ea47992790fdb2e722786508d148cc
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-02-13 12:41:39 +0100

    adding html elements in jsx #2 #4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
 
 const App = () => {
-  return <p>Crazy Calculator</p>;
+  return (
+    <div>
+      <p className="hidden">Crazy Calculator</p>
+      <section className="header">
+        <h2 className="title">Flower Arrangement</h2>
+        <p className="subtitle">Your personal bouquet of flowers, for every occasion</p>
+      </section>
+
+      <section className="form">
+        <h2 className="hidden">Flower Form</h2>
+        <label htmlFor="season">Choose a season</label>
+          <select name="seasons" id="select-season">
+              <option value="">--Please select the season--</option>
+              <option value="spring">Spring</option>
+              <option value="summer">Summer</option>
+              <option value="fall">Fall</option>
+              <option value="winter">Winter</option>
+          </select>
+
+      {/* via compontent checken welke bloemen er geselecteerd zijn in bovenstaande selectievenster */}
+      <label>Flower #1
+        <input type="number" id="quantity" name="quantity" defaultValue="10"></input>
+      </label>
+
+      {/* color picker om de vier verschillende kleuren te bepalen van het boeket, op basis van de gekozen kleuren, veranderen de kleuren onderaan */}
+        <label>Color #1
+          <input type="color" defaultValue="#F188C9"></input>
+        </label>
+      </section>
+      
+    </div>
+    
+  );
 };
 
 export default App;
