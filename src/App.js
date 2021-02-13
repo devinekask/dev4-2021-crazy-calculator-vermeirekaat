d93db39c2d9ef6f61be2a622c4d930c0c69d5334
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-02-13 13:52:54 +0100

    basic html & css #2 #4 #5

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ const App = () => {
       <p className="hidden">Crazy Calculator</p>
       <section className="header">
         <h2 className="title">Flower Arrangement</h2>
-        <p className="subtitle">Your personal bouquet of flowers, for every occasion</p>
+        <p className="slogan">Your personal bouquet, perfect for every occasion</p>
       </section>
 
       <section className="form">
         <h2 className="hidden">Flower Form</h2>
-        <label htmlFor="season">Choose a season</label>
+        <label className="label" htmlFor="season">Choose a season</label>
           <select name="seasons" id="select-season">
               <option value="">--Please select the season--</option>
               <option value="spring">Spring</option>
@@ -21,16 +21,31 @@ const App = () => {
           </select>
 
       {/* via compontent checken welke bloemen er geselecteerd zijn in bovenstaande selectievenster */}
-      <label>Flower #1
+      <label className="label">Flower #1
         <input type="number" id="quantity" name="quantity" defaultValue="10"></input>
       </label>
 
       {/* color picker om de vier verschillende kleuren te bepalen van het boeket, op basis van de gekozen kleuren, veranderen de kleuren onderaan */}
-        <label>Color #1
+        <label className="label">Color #1
           <input type="color" defaultValue="#F188C9"></input>
         </label>
       </section>
       
+      <section className="bouquet">
+        <h2 className="hidden">Bouquet Example</h2>
+          <article className="bouquet--small">
+            <h3 className="subtitle">Bouquet Small</h3>
+            <div className="placeholder placeholder--small"></div>
+          </article>
+          <article className="bouquet--medium">
+            <h3 className="subtitle">Bouquet Medium</h3>
+            <div className="placeholder placeholder--medium"></div>
+          </article>
+          <article className="bouquet--large">
+            <h3 className="subtitle">Bouquet Large</h3>
+            <div className="placeholder placeholder--large"></div>
+          </article>
+      </section>
     </div>
     
   );
