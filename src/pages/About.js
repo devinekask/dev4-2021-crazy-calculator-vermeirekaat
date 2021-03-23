d85ed9465ba41e35535ab75970c871534850ca0f
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-03-23 22:35:50 +0100

    adding content second page #31

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,27 @@
 import React from "react";
+import styles from "./About.module.css";
 
 const About = () => {
     return (
-        <p>About</p>
+        <article className={styles.poem}>
+            <h2 className={styles.title}>BloemGedicht</h2>
+            <div className={styles.part}>
+                <p>Ik noem je: <span>bloemen</span> </p>
+                <p>Ik noem je: merel in de vroegte</p>
+                <p>Ik noem je: <span>mooi</span> </p>
+            </div>
+            <div className={styles.part}>
+                <p>Ik noem je: <span>narcissen</span> in de nacht</p>
+                <p>waarover de wind strijkt</p>
+                <p>naar <span>mij</span> toe</p>
+            </div>
+            <div className={styles.part}>
+                <p>ik noem je: <span>bloemen in de nacht</span></p>
+            </div>
+            <div className={styles.writer}>
+                <p>Jan Hanlo (1912-1969)</p>
+            </div>
+        </article>
     )
 };
 
