8f2d4919116707786238056aeb5519cf0571d1cb
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-03-26 14:43:17 +0100

    delete copy that isn't needed

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,23 +1,21 @@
 import styles from "./Overview.module.css";
 
 const Overview = ({ list }) => {
-
-    const newList = [...list];
     
-    if(newList.length <= 0) {
+    if(list.length <= 0) {
         return (
             <div>
-                <h2 className={styles.subtitle}>Mijn Boeketten ({newList.length})</h2>
+                <h2 className={styles.subtitle}>Mijn Boeketten ({list.length})</h2>
                 <p className={styles.info}>Je hebt nog geen boeketten opgeslagen.</p>
             </div>         
         )
     }
-    if (newList.length === 1) {  
-        const flowers = newList[0].bouquet;
-        const name = newList[0].name;
+    if (list.length === 1) {  
+        const flowers = list[0].bouquet;
+        const name = list[0].name;
         return (
             <>
-            <h2 className={styles.subtitle}>Mijn Boeketten ({newList.length})</h2>
+            <h2 className={styles.subtitle}>Mijn Boeketten ({list.length})</h2>
             <div className={styles.part}>
                 <p className={styles.first}>{name}</p>
             <div className={styles.klein}>
@@ -34,16 +32,16 @@ const Overview = ({ list }) => {
            
         )
     }
-    if (newList.length >= 2 ) {
+    if (list.length >= 2 ) {
         return (
             <>
-            <h2 className={styles.subtitle}>Mijn Boeketten ({newList.length})</h2>
+            <h2 className={styles.subtitle}>Mijn Boeketten ({list.length})</h2>
                 <div className={styles.overview}>
 
-                 {newList.map((item) => (
+                 {list.map((item) => (
                 <div className={styles.part}>
-                        <p className={styles.name} key={`p ${newList.indexOf(item.name)}`}>{item.name}</p> 
-                    <div key={`div ${newList.indexOf(item.name)}`} className={styles.klein}>     
+                        <p className={styles.name} key={`p ${list.indexOf(item.name)}`}>{item.name}</p> 
+                    <div key={`div ${list.indexOf(item.name)}`} className={styles.klein}>     
                     {item.bouquet.map((flower) => (
                         <div key={item.bouquet.indexOf(flower)} className={styles.overlay} style={{backgroundColor: flower.color}}>
                             <img key={`div ${item.bouquet.indexOf(flower)}`}className={styles.image} src={flower.image} alt={flower.name} width="50"></img>
